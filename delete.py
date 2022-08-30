0d0093cbeaf57d0e54e21ce50124cb4bcafc623a
lucyfyy <bambang.nalann@gmail.com>
2022-08-30 16:16:22 +0700

    change baseexc

--- delete.py
+++ delete.py
@@ -9,6 +9,6 @@ def tmp(file):
               os.unlink(file_path)
           elif os.path.isdir(file_path):
               shutil.rmtree(file_path)
-      except Exception as e:
+      except BaseException as e:
           print('Failed to delete %s. Reason: %s' % (file_path, e))
           continue
