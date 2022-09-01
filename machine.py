e773fd16356bbfd9db24d0263a3c3c4fd88e35f4
lucyfyy <bambang.nalann@gmail.com>
2022-09-01 12:50:42 +0700

    fix ogg

--- machine.py
+++ machine.py
@@ -17,8 +17,8 @@ async def convert_ogg(dest_bucket_name, name):
     stream = ffmpeg.input("/tmp/{file}.mp4".format(file=name))
     ogg = ffmpeg.output(stream, '/tmp/{name}.ogg'.format(name=name))
     print("Converting to ogg")
-    await cloudstorage.upload(dest_bucket_name, f"{name}.ogg", f"{name}.ogg")
     ffmpeg.run(ogg)
+    await cloudstorage.upload(dest_bucket_name, f"{name}.ogg", f"{name}.ogg")
     print(f"File {name}.ogg successfully converted and uploaded to {dest_bucket_name}")
 
 async def create_thumbnail(dest_bucket_name, name):
